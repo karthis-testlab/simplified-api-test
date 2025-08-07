e276e45df622729c8151f3aa3fbf7651b7257ca0
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-08-07 13:32:00 +0530

    Update the interface

--- src/main/java/com/simplified/api/test/design/ApiClient.java
+++ src/main/java/com/simplified/api/test/design/ApiClient.java
@@ -1,11 +1,14 @@
 package com.simplified.api.test.design;
 
+import java.util.Map;
+
 import io.restassured.specification.RequestSpecification;
 
 public interface ApiClient {
 	
 	ResponseAPI get(RequestSpecification request, String endpoint);
 	ResponseAPI post(RequestSpecification request, String endpoint, Object payload);
+	ResponseAPI post(RequestSpecification request, Map<String, String> formParams);
 	ResponseAPI put(RequestSpecification request, String endpoint, Object payload);
 	ResponseAPI patch(RequestSpecification request, String endpoint, Object payload);
 	ResponseAPI delete(RequestSpecification request, String endpoint, Object payload);
