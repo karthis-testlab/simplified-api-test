542bdbf98db7438df6f2a946d706611e88bfdb4a
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-08-09 23:17:40 +0530

    ServiceNowBase.java create

--- src/test/java/com/simplified/api/test/servicenow/som/ServiceNowBase.java
+++ src/test/java/com/simplified/api/test/servicenow/som/ServiceNowBase.java
@@ -5,19 +5,17 @@ import com.simplified.api.test.design.ResponseAPI;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
 
 public class ServiceNowBase {
 	
 	protected ResponseAPI response;
 	protected RestAssuredBaseImpl restAssured = new RestAssuredBaseImpl();	
+	protected RequestSpecBuilder requestBuilder;
 	
-	public RequestSpecification commonSpec() {
+	public RequestSpecBuilder commonSpec() {
 		return new RequestSpecBuilder()
-				.setBaseUri("https://dev271917.service-now.com")
-				.setBasePath("/api/now/table")
-				.setAuth(RestAssured.basic("admin", "ay*lPG$Va97D"))
-				.build();
+				.setBaseUri("https://dev271917.service-now.com")				
+				.setAuth(RestAssured.basic("admin", "ay*lPG$Va97D"));
 	}
 
 }
