f3aec25f134789a452578794b0286e89f5d1aed2
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-08-13 22:46:03 +0530

    Commited on 130820251045

--- src/test/java/com/simplified/api/test/servicenow/som/OAuthService.java
+++ src/test/java/com/simplified/api/test/servicenow/som/OAuthService.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.json.JSONObject;
 
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
@@ -18,10 +19,10 @@ public class OAuthService extends ServiceNowBase {
 	public OAuthService generateToken() {
 		Map<String, String> forms = new HashMap<String, String>();
 		forms.put("grant_type", "password");
-		forms.put("client_id", "970096e343c3221040322689d033ce18");
-		forms.put("client_secret", "$!hXpkhMfb");
+		forms.put("client_id", "ae91a5e138d322103203abcc4d110258");
+		forms.put("client_secret", "MPys?&7~O(");
 		forms.put("username", "admin");
-		forms.put("password", "ay*lPG$Va97D");
+		forms.put("password", "dLS9Qgs=V!0n");
 		response = restAssured.post(requestBuilder.setContentType(ContentType.URLENC).build(), forms);
 		return this;
 	}
@@ -36,6 +37,11 @@ public class OAuthService extends ServiceNowBase {
 	public String extractToken(String jsonPath) {
 		JsonPath json = new JsonPath(response.getBody());
 		return json.getString(jsonPath);
+	}
+	
+	public String extractToken() {
+		JSONObject json = new JSONObject(response.getBody());
+		return json.getString("access_token");
 	}
 
 }
